1a82a938f7235f64c66cb66c59eba284f7e88c4e
Alex Riley <alex.riley.ar12@gmail.com>
2021-08-12 14:42:27 -0500

    added the external js file

---
+++ js/external.js
@@ -0,0 +1,10 @@
+
+"use strict";
+
+// console.log("Hello from external js")
+// var myFirstStatement = "This is sooo cool!"
+// console.log(myFirstStatement)
+
+var userDinner = prompt("What are you going to have for dinner today?")
+
+alert("The user is having " + userDinner + " for dinner")
