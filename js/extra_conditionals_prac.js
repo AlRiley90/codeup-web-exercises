441cac9130aa51c1ba931707c93cbece05e06562
Alex Riley <alex.riley.ar12@gmail.com>
2021-08-16 20:58:13 -0500

    Started w3 resource exercises for practice - Conditionals

---
+++ js/extra_conditionals_prac.js
@@ -0,0 +1,24 @@
+"use strict"
+//w3resource first problem
+function biggerNumber(a, b){
+    if(a > b){
+        console.log(a + " is the bigger of the two numbers")
+    }else if(a < b){
+        console.log(b + " is the bigger of the two numbers")
+    } else{
+        console.log("The two numbers have the same value")
+    }
+}
+biggerNumber()
+//w3resource second problem
+function signBeforeNumber(x){
+    if(x > 0){
+        console.log("The sign in front of the number is +");
+    } else if(x < 0){
+        console.log("The sign in front of the number is -")
+    } else {
+        console.log("Zero is neither negative nor positive")
+    }
+}
+signBeforeNumber()
+//w3resource third problem
