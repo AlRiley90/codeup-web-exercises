86060e924943c93602a9755b472fc40cb2e2c071
Alex Riley <alex.riley.ar12@gmail.com>
2021-08-18 11:33:11 -0500

    Updated all exercises except the Do While loops

---
+++ js/do_while.js
@@ -0,0 +1,7 @@
+"use strict"
+function randomNumber(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+console.log(randomNumber(50, 100));
--- js/for_loops.js
+++ js/for_loops.js
@@ -7,7 +7,7 @@ function showMultiplicationTable(num, i){
         console.log(num + " x " + i + " = " + product);
         i++;
     } while (i <= 10);
-}
+}//showMultiplicationTable(5)
 function isEvenOrOdd() {
     for (var i = 0; i < 10; i++) {
         var randomInt = Math.floor(Math.random() * (200 - 20 + 1) + 20);
@@ -17,20 +17,22 @@ function isEvenOrOdd() {
             console.log(randomInt + " is an odd number")
         }
     }
-}
+}// isEvenOrOdd();
 // console.log(isEvenOrOdd());
 // //Question 4 of the exercise need to ask for help
-var addThis = 1;
-for(var i = "1"; i.length < 10; i = i + addThis) {
 
-    console.log(i);
-    addThis++;
-}
-
-var i;
-for (i = 100; i > 0; i -= 5){
-    if(i === 0){
-        break;
+for(var i = 1; i.length < 10; i++) {
+    var answer = "";
+    for(var j = 1; j <= i; j++){
+        answer += i;
     }
-    console.log(i);
+    console.log(answer);
 }
+
+// var i;
+// for (i = 100; i > 0; i -= 5){
+//     if(i === 0){
+//         break;
+//     }
+//     console.log(i);
+// }
