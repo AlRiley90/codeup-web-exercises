3c1f2f7ba86fe6b2b6df7b58430b0b80cd30a1e7
Alex Riley <alex.riley.ar12@gmail.com>
2021-08-18 06:16:16 -0500

    Completed the while loop exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -6,12 +6,15 @@
 //     }
 // }
 // console.log("Is this working")
-var userOddNumber = prompt("Please enter and odd number between 1 and 50")
-
-    while (userOddNumber % 2 === 0 || userOddNumber > 50 || userOddNumber < 1) {
-        prompt("Please try again");
-        if (userOddNumber % 2 !== 0) {
-            alert("Thank you for entering a valid number!")
-            break;
-        }
+var userOddNumber = prompt("Please enter and odd number between 1 and 50", "0")
+var num = parseInt(userOddNumber)
+    while (num % 2 === 0 || num > 50 || num < 1) {
+    prompt("Please try again")
+    if (num % 2 !== 0) {
+        alert("Thank you for entering a valid number!")
+        break;
+    }
 }
+
+
+    //Question 3
---
+++ js/while.js
@@ -0,0 +1,8 @@
+"use strict"
+
+var x = 2;
+
+while(x < 100000){
+   console.log(x);
+   x = x * 2;
+}
