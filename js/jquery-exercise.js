457eed7720d99833279b5b9a950e9d2a1fcf440b
Alex Riley <alex.riley.ar12@gmail.com>
2021-09-28 15:26:58 -0500

    added click and hover functionality

--- js/jquery-exercise.js
+++ js/jquery-exercise.js
@@ -21,3 +21,34 @@ $("html").css("border", "20px solid black");
 $(".sked").css("text-align", "center");
 
 $("img").css("padding-left", "30px");
+
+$("span").css("display", "none");
+
+$("h1").click(function(){
+    // if("background-color"=== "red") {
+        $(this).css("background-color", "black")
+    // }else{
+    //     $(this).css("background-color", "red")
+    // }
+});
+
+$("li").click(function(){
+        $(this).css("color", "black").css("font-size", "40px");
+        $("span").css("display", "inherit");
+});
+
+// $("h1").click(function(){
+//     $(this).css("background-color", "red")
+// });
+$(".logo").dblclick(function(){
+     $(this).css("border", "2px solid lightgray")
+});
+
+$("li").hover(
+    function() {
+        $(this).css("font-size", "40px");
+    },
+    function(){
+            $(this).css("font-size", "inherit");
+    }
+    );
