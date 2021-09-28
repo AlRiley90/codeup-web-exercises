5e99086e21596a0c3de0a96abd1bbcb8376531fa
Alex Riley <alex.riley.ar12@gmail.com>
2021-09-28 13:37:29 -0500

    finished second jquery exercise/Ga Bulldogs page

--- js/jquery-exercise.js
+++ js/jquery-exercise.js
@@ -1,7 +1,20 @@
 "use strict";
 
-$(document).ready(function() {
+var content = $("#title2").html();
 
-    alert("Document is fully loaded!")
+    // alert(content);
 
-});
+$(".codeup").css("border", "1px solid red");
+
+$("div").css("background", "red").css("color", "white");
+
+$("#title2").css("color", "black").css("text-align", "center");
+
+$("main").css("background", "lightgrey");
+
+$("#title").css("text-align", "center")
+
+$("li").css("font-size", "20px").css("list-style", "none");
+
+$("html").css("border", "20px solid black");
+
