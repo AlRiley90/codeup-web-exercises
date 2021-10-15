0f2dad7060c50c7c9796b608733bdbb54b098583
Alex Riley <alex.riley.ar12@gmail.com>
2021-10-15 12:01:48 -0500

    completed portion that requests users GitHub username to provide their last commit

--- js/promises.js
+++ js/promises.js
@@ -9,7 +9,16 @@ fetch('https://api.github.com/users/AlRiley90/events', {
                 return jsonData[0].created_at
             })
 
-
-
+function userName(){
+    let input = prompt("Please enter your GitHub username: ");
+    fetch('https://api.github.com/users/' + input +'/events', {
+        headers: {'Authorization': gitHubKey}
+    })
+        .then((response) => response.json())
+        .then((jsonData) => {
+            console.log(jsonData[0].created_at)
+})
+}
+userName();
 
 
